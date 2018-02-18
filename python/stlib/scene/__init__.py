14327bc835ec7e01624be6b32e4c535f67dfea7e
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-02-18 21:51:30 +0100

    [STLIB] Add a Node() template.

--- python/stlib/scene/__init__.py
+++ python/stlib/scene/__init__.py
@@ -8,13 +8,19 @@ Content:
 
     MainHeader
     ContactHeader
+    Node
 
 |
 
 .. autofunction:: MainHeader
 .. autofunction:: ContactHeader
+.. autofunction:: Node
 
 """
 
 from mainheader import MainHeader
 from contactheader import ContactHeader
+
+def Node(parentNode, Name):
+    """Create a new node and attach it to the provided node"""
+    return parentNode.createChild(Name)
