e038fede0035f2cf9e0dd65616c0eaa23760dda1
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-04-16 17:20:48 +0200

    [STLIB] ADD visuals.VisualModel (newstyle)

--- python/stlib/visuals/__init__.py
+++ python/stlib/visuals/__init__.py
@@ -2,7 +2,43 @@
 """
 Templates for rendering.
 """
+from splib.objectmodel import SofaPrefab, SofaObject
+from stlib.scene import Node
+
 __all__=[]
 
 def ShowGrid(node):
     node.createObject("OglGrid", nbSubdiv=10, size=1000)
+
+
+@SofaPrefab
+class VisualModel(SofaObject):
+    """VisualModel Prefab
+
+       This prefab is creating a VisualModel.
+
+       Arguments:
+            parent
+            surfaceMeshFileName
+            color
+
+       Content:
+           Node
+           {
+                name : 'Visual'
+                MeshLoader : 'loader'
+                OglModel : "model'
+           }
+    """
+    def __init__(self, parent, surfaceMeshFileName, color=[1.0,1.0,1.0]):
+        self.node  = Node(parent, "VisualModel")
+
+        self.loader = self.node.createObject('MeshSTLLoader',
+                                        name="loader",
+                                        filename=surfaceMeshFileName)
+
+        self.model = self.node.createObject('OglModel',
+                                        name="model",
+                                        position='@loader.position',
+                                        triangles='@loader.triangles',
+                                        color=color)
