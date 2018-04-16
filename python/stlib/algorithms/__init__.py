10fdf2f40b1f48dc9e5a429e6979b5eec38f9d7f
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-04-16 17:24:22 +0200

    [STLIB] FIX deprecation message for stlib.algorithms

--- python/stlib/algorithms/__init__.py
+++ python/stlib/algorithms/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
 import Sofa
 
-Sofa.msg_error("splib.algorithm", "The algorithm framework is now part of the splib instead of stlib. Please update your code base")
-
-from splib.algorithms import *
+raise Exception("The algorithm framework is now part of the splib instead of stlib. Please update your code base")
