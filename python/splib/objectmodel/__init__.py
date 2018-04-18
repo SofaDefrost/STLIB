16475966621711b0e43a52b0f4fd43a5921a644e
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-04-18 23:57:43 +0200

    [SPLIB] Simplify a lot the objectmodel
    This is because I made a small patch in Sofa to ease the things.

--- python/splib/objectmodel/__init__.py
+++ python/splib/objectmodel/__init__.py
@@ -1,57 +1,61 @@
 from inspect import currentframe, getframeinfo,getdoc
-import types 
+import types
+import Sofa
+
 
 class SofaPrefab(object):
     def __init__(self, cls):
         frameinfo = getframeinfo(currentframe().f_back)
-        self.cls = cls 
+        self.cls = cls
         self.definedloc = (frameinfo.filename, frameinfo.lineno)
-          
+
     def __call__(self, *args, **kwargs):
-        o = self.cls(*args, **kwargs)
-        frameinfo = getframeinfo(currentframe().f_back)
-  
-        o.node.addNewData("Prefab type", "Infos", "","s", str(o.__class__.__name__))
-        o.node.addNewData("Defined in", "Infos", "","s", str(self.definedloc))
-        o.node.addNewData("Instantiated in", "Infos", "","s", str((frameinfo.filename, frameinfo.lineno)))
-        o.node.addNewData("Help", "Infos", "", "s", str(getdoc(o)))
-        return o        
+            o = self.cls(*args, **kwargs)
+            frameinfo = getframeinfo(currentframe().f_back)
+            o.node.addNewData("Prefab type", "Infos", "","s", str(o.__class__.__name__))
+            o.node.addNewData("Defined in", "Infos", "","s", str(self.definedloc))
+            o.node.addNewData("Instantiated in", "Infos", "","s", str((frameinfo.filename, frameinfo.lineno)))
+            o.node.addNewData("Help", "Infos", "", "s", str(getdoc(o)))
+
+            ## This is the kind of hack that make me love python
+            def sofaprefab_getattr(self, name):
+                return getattr(self.node, name)
+
+            setattr(self.cls, "__getattr__", sofaprefab_getattr)
+            return o
 
     def __getattr__(self, name):
-        ## This one forward query to the decorated class. This is usfull to access static method of the object.       
+        ## This one forward query to the decorated class. This is usfull to access static method of the object.
         return getattr(self.cls, name)
 
 class SofaObject(object):
     def __init__(self, node, name):
-        self.node = node.createChild(name)   
-        self.loader = None
-        self.model = None
-        self.dofs = None
-        self.mapping = None
-        self.visual = None
-        self.collision = None
+        self.node = node.createChild(name)
 
     def __getattr__(self, name):
-        tmp = self.node.getData(name)
-        if tmp == None:
-            t = self.node.getChild(name, warning=False)
-            if t != None:
-                tmp = SofaObjectWrapper(t)
-        if tmp == None:
-            tmp = self.node.getObject(name, warning=False)
-        if tmp == None:
-            raise Exception("Missing attribute '"+name+"' in "+str(self) )
+        return getattr(self.node, name)
 
-        return tmp
+        #tmp = self.node.getData(name)
+        #if tmp == None:
+        #    t = self.node.getChild(name, warning=False)
+        #    if t != None:
+        #        tmp = SofaObjectWrapper(t)
+        #if tmp == None:
+        #    tmp = self.node.getObject(name, warning=False)
+        #if tmp == None:
+        #    raise Exception("Missing attribute '"+name+"' in "+str(self) )
 
-    def createChild(self, name):
-        return self.node.createChild(name)
-    
-    def createObject(self, *args, **kwargs):
-        return self.node.createObject(*args, **kwargs)
+        #return tmp
+
+    #def createChild(self, name):
+    #    return self.node.createChild(name)
+
+    #def createObject(self, *args, **kwargs):
+    #    return self.node.createObject(*args, **kwargs)
 
 class SofaObjectWrapper(object):
     def __init__(self, node):
+        print("DEPRECATED.... SOFAOBJECTWRAPPER")
         self.node = node
 
     def createChild(self, name):
@@ -61,7 +65,7 @@ class SofaObjectWrapper(object):
         return self.node.createObject(*args, **kwargs)
 
     def __getattr__(self, name):
-        
+
         tmp = self.node.getData(name)
         if tmp == None:
             t = self.node.getChild(name, warning=False)
