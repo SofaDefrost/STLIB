fc109e73ddc5182433b179752bb05d6f754dd397
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-02-11 14:26:01 +0100

    [STLIB] Add the 'Rigidify' template in stlib.physics.mixedmaterial
    And example is provided in the file rigidification.py

---
+++ python/stlib/physics/mixedmaterial/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+Templates for rigidify a deformable object.
+Rigidification consist in mixing rigid and deformable parts interacting together.
+
+**Content:**
+
+.. autosummary::
+
+   Rigidify
+
+stlib.physics.mixedmaterial.Rigidify
+**********************************************
+.. autoclass:: Rigidify
+    :members:
+    :undoc-members:
+
+"""
+from rigidification import Rigidify
+
