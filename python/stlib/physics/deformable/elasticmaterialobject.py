6a98d5c4f1f7078fc7cd50e84f7946f9157cc488
COEVOET Eulalie <eulalie.coevoet@gmail.com>
2019-03-29 16:19:11 +0100

    [stlib.physics] change the way RequiredPlugin is added (following D.Marchal review)

--- python/stlib/physics/deformable/elasticmaterialobject.py
+++ python/stlib/physics/deformable/elasticmaterialobject.py
@@ -8,6 +8,7 @@ from stlib.visuals import VisualModel
 @SofaPrefab
 class ElasticMaterialObject(SofaObject):
     """Creates an object composed of an elastic material."""
+
     def __init__(self,
                  attachedTo=None,
                  volumeMeshFileName=None,
@@ -43,7 +44,9 @@ class ElasticMaterialObject(SofaObject):
                      youngModulus=18000,
                      totalMass=1.0, solver=None):
 
-        self.createObject("RequiredPlugin", name="SofaSparseSolver")
+        if not self.getRoot().getObject("SofaSparseSolver", warning=False):
+            Sofa.msg_info("Missing RequiredPlugin SofaSparseSolver in the scene, add it from Prefab ElasticMaterialObject.")
+            self.getRoot().createObject("RequiredPlugin", name="SofaSparseSolver")
 
         if self.node is None:
             Sofa.msg_error("Unable to create the elastic object because it is not attached to any node. Please fill the attachedTo parameter")
