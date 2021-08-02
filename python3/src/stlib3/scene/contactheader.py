92ea7a46c3808bb2e590cda3c7aa5108f9a1531c
unknown <Corentin Languepin>
2021-08-02 14:44:08 +0200

    à tester

--- python3/src/stlib3/scene/contactheader.py
+++ python3/src/stlib3/scene/contactheader.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-def ContactHeader(applyTo, alarmDistance, contactDistance, frictionCoef=0.0):
+def ContactHeader(rootNode, alarmDistance, contactDistance, frictionCoef=0.0):
     '''
     Args:
-        applyTo (Sofa.Node): the node to attach the object to
+        rootNode (Sofa.Node): the node to attach the object to
 
         alarmDistance (float): define the distance at which the contact are integrated into
                                the detection computation.
@@ -25,24 +25,24 @@ def ContactHeader(applyTo, alarmDistance, contactDistance, frictionCoef=0.0):
                 LocalMinDistance
             }
     '''
-    if applyTo.getObject("DefaultPipeline", warning=False) is None:
-            applyTo.addObject('DefaultPipeline')
+    if rootNode.hasObject("DefaultPipeline", warning=False) is None:
+            rootNode.addObject('DefaultPipeline')
 
-    applyTo.addObject('BruteForceDetection')
+    rootNode.addObject('BruteForceDetection')
 
-    applyTo.addObject('RuleBasedContactManager', responseParams="mu="+str(frictionCoef),
+    rootNode.addObject('RuleBasedContactManager', responseParams="mu="+str(frictionCoef),
                                                     name='Response', response='FrictionContact')
-    applyTo.addObject('LocalMinDistance',
+    rootNode.addObject('LocalMinDistance',
                         alarmDistance=alarmDistance, contactDistance=contactDistance,
                         angleCone=0.01)
 
-    if applyTo.getObject("FreeMotionAnimationLoop", warning=False) is None:
-            applyTo.addObject('FreeMotionAnimationLoop')
+    if rootNode.hasObject("FreeMotionAnimationLoop", warning=False) is None:
+            rootNode.addObject('FreeMotionAnimationLoop')
 
-    if applyTo.getObject("GenericConstraintSolver", warning=False) is None:
-            applyTo.addObject('GenericConstraintSolver', tolerance=1e-6, maxIterations=1000)
+    if rootNode.hasObject("GenericConstraintSolver", warning=False) is None:
+            rootNode.addObject('GenericConstraintSolver', tolerance=1e-6, maxIterations=1000)
 
-    return applyTo
+    return rootNode
 
 ### This function is just an example on how to use the DefaultHeader function.
 def createScene(rootNode):
