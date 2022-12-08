489aa9e5d0a632b37572f64b672e266cc914d0e8
CHAILLOU Paul <paul.chaillou@inria.fr>
2022-12-08 16:55:01 +0100

    1st commit on my fork

---
+++ python3/src/splib3/topology/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+.. autosummary::
+    :toctree: _autosummary
+
+    splib3.algorithms
+    splib3.animation
+    splib3.debug
+    splib3.geometric
+    splib3.loaders
+    splib3.numerics
+    splib3.objectmodel
+    splib3.scenegraph
+    splib3.units
+
+"""
