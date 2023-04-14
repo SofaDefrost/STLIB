a3d3aaca3c9893267da61a70aad8268ab4f3dbe2
eulalie <eulalie.coevoet@gmail.com>
2023-04-14 17:34:34 +0200

    [splib3] serialport adds check

--- python3/src/splib3/interface/serialport.py
+++ python3/src/splib3/interface/serialport.py
@@ -11,12 +11,19 @@ def getDevicePort(entry, method="manufacturer"):
      - serial_number
     """
     ports = []
+    comports = serial.tools.list_ports.comports()
+
+    if comports is None:
+        Sofa.msg_error("stlib3.interface.serialport",
+                       "Serial ports check failed, list of ports is empty.")
+        return
+
     if method == "manufacturer":
-        ports = [p for p in serial.tools.list_ports.comports() if entry in p.manufacturer]
+        ports = [p for p in comports if entry in p.manufacturer]
     if method == "description":
-        ports = [p for p in serial.tools.list_ports.comports() if entry in p.description]
+        ports = [p for p in comports if entry in p.description]
     if method == "serial_number":
-        ports = [p for p in serial.tools.list_ports.comports() if entry in p.serial_number]
+        ports = [p for p in comports if entry in p.serial_number]
 
     if not ports:
         Sofa.msg_error("stlib3.interface.serialport",
